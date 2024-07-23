3dfe0975f266ac60bc47a6113dba115cacba014e
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-07-23 12:06:15 +0000

    Datatypes

---
+++ basic_in_js/01_practicejsvariables.js
@@ -0,0 +1,14 @@
+const account_Id=123354;
+let account_email="abc@gmail.com";
+var acc_pass="123456Av";
+city="JSR";
+
+// account_Id=5464; not allowed
+account_email="hfc@gmail.com";
+acc_pass="565434354"
+city="Bangalore";
+
+let acc_State;
+console.log(account_Id);
+
+console.table([account_Id,account_email,acc_pass,city,acc_State])
---
+++ basic_in_js/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3+3) we are using node js not browser 
+
+console.log(3 + 6);
+
+
+let name="javascript";
+let age=18;
+let isLoggeedIn=false;
+
+// 1)number
+// 2)bigint
+// 3)string
+// 4)boolean =>true/false
+// 5)null => standalone value
+// 6) undefined => not defined
+
+
+// object
+console.log(typeof undefined); // type is undefined
+console.log(typeof null); // type is object
