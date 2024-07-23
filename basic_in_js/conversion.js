aad773fec80f11f167e2d8acead693abe5b2b174
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-07-23 12:41:52 +0000

    conversion

---
+++ basic_in_js/conversion.js
@@ -0,0 +1,39 @@
+// let score=true;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valInNos=Number(score);
+// console.log(valInNos);
+
+// "33" => 33 from `Number` function
+// "23adf"=>NaN
+// true =>1; false=>0;
+
+let isLoggedIn="fdy";
+let booleanisLoggedIn=Boolean(isLoggedIn);
+
+console.log(booleanisLoggedIn);
+
+// 1=>true;0=>false;
+// ""=>false
+// "asdad'=>true
+ 
+
+let anyNumber=6546546;
+let stringConvert=String(anyNumber);
+console.log(typeof stringConvert);
+
+
+
+let myNubercheck=6544;
+let check=String(myNubercheck);
+console.log(check);
+
+let checkbool=true;
+let checkboolean=Boolean(checkbool);
+console.log(typeof checkboolean);
+
+
+
