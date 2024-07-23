2b0a69b40eced9e17a4412c1eb300b5becb61d93
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-07-23 13:18:49 +0000

    prefix

--- basic_in_js/conversion.js
+++ basic_in_js/conversion.js
@@ -11,29 +11,63 @@
 // "23adf"=>NaN
 // true =>1; false=>0;
 
-let isLoggedIn="fdy";
-let booleanisLoggedIn=Boolean(isLoggedIn);
+let isLoggedIn = "fdy";
+let booleanisLoggedIn = Boolean(isLoggedIn);
 
 console.log(booleanisLoggedIn);
 
 // 1=>true;0=>false;
 // ""=>false
 // "asdad'=>true
- 
 
-let anyNumber=6546546;
-let stringConvert=String(anyNumber);
+
+let anyNumber = 6546546;
+let stringConvert = String(anyNumber);
 console.log(typeof stringConvert);
 
 
 
-let myNubercheck=6544;
-let check=String(myNubercheck);
+let myNubercheck = 6544;
+let check = String(myNubercheck);
 console.log(check);
 
-let checkbool=true;
-let checkboolean=Boolean(checkbool);
+let checkbool = true;
+let checkboolean = Boolean(checkbool);
 console.log(typeof checkboolean);
 
+// ********Operation**********
+
+// let val=3;
+// let negativeVal=-val;
+// console.log("Operation",negativeVal);
+
+// console.log("Operation 2",2**3);
 
 
+
+// let str1="hello";
+// let str2="Hitesh";
+
+// let str3 =str1+str2;
+// console.log("str3",str3);
+
+// -----------Not Legal----------
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(2+2+"1");//41
+
+// console.log(+true); 
+// console.log(+"");
+
+// -----------Not Legal----------
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+// prefix and postfix
+let gamecounter =100;
+++gamecounter;
+console.log("gamecounter",gamecounter);
