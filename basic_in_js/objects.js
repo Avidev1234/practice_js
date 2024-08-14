2a98311a547b9e9289508632cfa2562c96a38442
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-08-14 04:40:52 +0000

    objects

--- basic_in_js/objects.js
+++ basic_in_js/objects.js
@@ -2,16 +2,16 @@
 // Object.create
 
 //---------------------------- object literals----------------------------------
-const mySymbol=Symbol("sdfsdfdfsfaf");
-const js_user={
-    name:"Avidev",
-    age:52,
-    [mySymbol]:"Avidev Jha",
-    "fullName":"sadasds",
-    location:"JSR",
-    email:"avi@gmail.com",
-    isLoggedIn:false,
-    lastLoggindays:["Monday","Saturday"]
+const mySymbol = Symbol("sdfsdfdfsfaf");
+const js_user = {
+    name: "Avidev",
+    age: 52,
+    [mySymbol]: "Avidev Jha",
+    "fullName": "sadasds",
+    location: "JSR",
+    email: "avi@gmail.com",
+    isLoggedIn: false,
+    lastLoggindays: ["Monday", "Saturday"]
 };
 
 // console.log(js_user["age"]);
@@ -25,20 +25,20 @@ const js_user={
 // js_user.email="avidevj@gmail.com";
 // Object.freeze(js_user);//freeze 
 
-js_user.email="avidevjha9@gmail.com";
+js_user.email = "avidevjha9@gmail.com";
 console.log(js_user.email);
 
 
 // console.log(js_user);
 
 
-js_user.greetings=function (params) {
+js_user.greetings = function (params) {
     console.log("Hello everyone!!");
-    
+
 }
-js_user.greetingsTwo=function (params) {
+js_user.greetingsTwo = function (params) {
     console.log(`Hello everyone!!, my age is ${this.age}`);
-    
+
 }
 
 console.log(js_user.greetings());
@@ -48,11 +48,41 @@ console.log(js_user.greetingsTwo());
 // --------------------constructor/singleton-----------------------
 
 // const newObj=new Object();
-const newObj ={}
+const newObj = {}
 
- newObj.id="123abcd";
- newObj.name="Sam";
- newObj.age="54";
- newObj.isLoggedOut="Sunday";
- newObj.time="10:00AM"
-console.log(newObj);
+newObj.id = "123abcd";
+newObj.name = "Sam";
+newObj.age = "54";
+newObj.isLoggedOut = "Sunday";
+newObj.time = "10:00AM"
+// console.log(newObj);
+
+const regUser = {
+    email: "reguser@gmail.com",
+    fullName: {
+        userName: {
+            firstName: "Aman",
+            lastName: "kumar"
+        }
+    }
+}
+
+// console.log(regUser.fullName.userName);
+
+const obj_1={
+    1:"abc",
+    2:"efg"
+}
+const obj_2={
+    3:"three",
+    4:"four"
+}
+const obj_4={
+    5:"five",
+    6:"six"
+}
+
+// const obj_3={obj_1,obj_2};
+// const obj_3=Object.assign({},obj_1,obj_2,obj_4);
+const obj_3={...obj_1,...obj_2,...obj_4}
+console.log(obj_3);
