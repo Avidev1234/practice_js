5f9db7544a39940b7da5e1b4886b8a675063fef9
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-08-23 09:14:16 +0000

    function_updates

--- function/functions.js
+++ function/functions.js
@@ -20,12 +20,39 @@ handleObject({
     price: 399
 })
 
+// or 
+// handleObject(user)
 //----------------------------- For Arrays------------------------
 
-const myNewArray = [100, 200, 300, 400];
+const myNewArray = [1000, 200, 300, 400];
 
 function returnSecondValue(getArray) {
     return getArray[0]
 }
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([800, 900, 1000, 2000]));
+// or
+// console.log(returnSecondValue([800, 900, 1000, 2000]));
+
+
+
+// --------------------------------X----------------------------
+// Normal function
+function AddNum(num1, num2) {
+    // console.log(num1+num2);
+    return num1 + num2;
+}
+let res = AddNum(10, 10);
+console.log(res);
+
+function loginuserMessage(userName = "anyuser") {
+    if (!userName) {
+        console.log("Please enter username properly");
+        return
+    }
+    return `${userName} just logged in`
+}
+console.log(loginuserMessage("Aman"));
+// console.log(loginuserMessage());
+
+
+
