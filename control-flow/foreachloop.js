c20be2ffe986aa65ac4fdf4080f889ba70109940
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-08-31 13:11:53 +0000

    foreachloop

---
+++ control-flow/foreachloop.js
@@ -0,0 +1,43 @@
+const lang = ["js", "cpp", "php", "css"];
+
+// lang.forEach( function (item){
+//     console.log(item);
+
+// } )
+
+// lang.forEach((val)=>{
+// console.log(val);
+// })
+
+// function print(items) {
+//     console.log(items);
+
+// }
+
+// lang.forEach(print)
+
+// lang.forEach((items,index,arr)=>{
+//     console.log(items,index,arr);
+// })
+
+
+const mycoding = [
+    {
+        language: "javascript",
+        languageFilename: "js"
+    },
+    {
+        language: "python",
+        languageFilename: "py"
+    },
+    {
+        language: "java",
+        languageFilename: "java"
+    },
+
+]
+
+
+mycoding.forEach((itemscoding)=>{
+console.log(itemscoding.language);
+})
