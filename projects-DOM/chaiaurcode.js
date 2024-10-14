a28d52b66fe233bb51f4e9cffba3edd790aa4659
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-10-14 13:20:10 +0000

    DOM_project-1

---
+++ projects-DOM/chaiaurcode.js
@@ -0,0 +1,30 @@
+const button=document.querySelectorAll('.button');
+// console.log(button);
+
+const body=document.querySelector("body");
+console.log(body);
+const head=document.querySelector("h1");
+// console.log(head);
+
+button.forEach((btn)=>{
+    console.log(btn);
+    btn.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id==='grey') {
+            body.style.backgroundColor=e.target.id;
+            head.style.color="white";
+            // body.style.backgroundColor="grey";
+        } else if (e.target.id ==='white') {
+            body.style.backgroundColor=e.target.id;
+            head.style.color="black";
+        }else if(e.target.id==="blue"){
+            body.style.backgroundColor=e.target.id;
+            head.style.color="white";
+        }else{
+            body.style.backgroundColor="yellow";
+            head.style.color="black";
+        }
+        
+    })
+})
