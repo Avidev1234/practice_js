ff840f918aac7500ede0fb9aaea45b5e2c141979
Avidev1234 <110378319+Avidev1234@users.noreply.github.com>
2024-11-19 14:51:16 +0000

    constructor

--- advance_js/oops.js
+++ advance_js/oops.js
@@ -1,24 +1,56 @@
 // object literal
 
-const user={
-    userName:"Aman",
-    loginCount:8,
-    signedIn:true,
-    
-    getUserDetails:function(){
-    //    return "user Details";
+const user = {
+    userName: "Aman",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function () {
+        //    return "user Details";
         // console.log("user Details");
-        console.log(`username:${this.userName}`);
-        console.log(this);
-        
-        
+        // console.log(`username:${this.userName}`);
+        // console.log(this);
+
+
     }
 }
 
-// console.log(user.userName);
+//console.log(user.userName);
 // console.log(user.getUserDetails());
+// console.log(this);{}
+
+
+// constructor Function:-
+
+// here `new` keyword is the constructor function which will create multiple instance from single object
 
 
 // const promiseOne=new Promise();
 // const date=new Date();
 // const arr=new Array()
+
+
+//function has only one Global Execution Context (GEC)
+function User(username, loginCount, isLoggedIn) {
+    this.myusername = username;//assigning username to myusername
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greetings=function(){
+        console.log(`welcome ${this.userName}`);
+        
+    }
+    return this; (optional)
+}
+
+const userOne = new User("Avidev", 200, true);
+const userTwo = new User("Alok", 45, 'true');
+console.log(userOne);
+console.log(userTwo);
+
+// conclusion of constructor section is if we don't use constructor it gives the latest code what ever we try to  `console` it will override the code from old one to new and if we use `new` keyword there will be new instance created.
+
+// `new` keyword create empty object
+// constructor function is being called due to new keyword and it pass the all different argument.
+// using `this` keyword pass the arguments
+
---
+++ advance_js/oops.md
@@ -0,0 +1,27 @@
+#javascript & classes
+
+## OOP
+
+## Object
+- collection of properties and methods
+- toLowercase
+
+## why use oop
+
+->Because the code become Spaghetti code.
+->Spaghetti code is a term used to describe code that is tangled, unorganized, and difficult to follow or maintain. It typically refers to software with a complex and disordered structure, where the logic flows in a chaotic, non-linear manner, similar to a plate of spaghetti. 
+
+## parts of oop
+Object Literal
+
+->constructor function
+->prototype
+->classes
+->Instances (new,this)
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
